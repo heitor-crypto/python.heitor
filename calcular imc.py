8e8592fc6821bde70eaa644bbe42ad90f49713b9
heitor-crypto <xavierheitor043@gmail.com>
2024-09-12 18:14:42 -0300

    Update calcular imc.py

--- calcular imc.py
+++ calcular imc.py
@@ -3,9 +3,11 @@ peso = float(input("digite o seu peso: "))
 altura = float(input("digite a sua altura: "))
 imc = peso / (altura ** 2)
 print("seu imc é {:.2f}".format(imc))
-if (peso <= 18,5 ):
+if peso < 18.5 :
     print("abaixo do peso")
-elif (peso in 18.5 and 24.9):
+elif peso <= 18.5 < 24.9 :
     print("peso normal")
+elif peso <= 25 < 34.9 :    
+    print("sobrepeso")
 else:
     print("acima do peso")
