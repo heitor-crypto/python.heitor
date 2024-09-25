5cc2b6d3b4265feec27074a4fd55e95f31be086d
heitor-crypto <xavierheitor043@gmail.com>
2024-09-25 17:15:17 -0300

    Create repetição.py

---
+++ repetição.py
@@ -0,0 +1,6 @@
+numero = int(input("digite um valor: "))
+multiplicador = 1
+while multiplicador <= 10:
+    resultado = multiplicador * numero
+    print(f"{multiplicador} x {numero} = {resultado}")
+    multiplicador += 1
