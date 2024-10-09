d48904fbeac6e56362531c82fbd99d752ff04058
heitor-crypto <xavierheitor043@gmail.com>
2024-10-09 17:29:58 -0300

    controle de fluxo de numeros

---
+++ controle de fluxo numeros.py
@@ -0,0 +1,14 @@
+contador = 0
+
+while contador <= 100:
+    contador +=1
+
+    if (contador == 6):
+        print("não vou mostrar o 6")
+        continue       
+    if (contador >= 10 and contador <= 27):
+        print ("não vou mostrar ")
+        continue
+    print(contador)
+    if contador == 40:
+        break
